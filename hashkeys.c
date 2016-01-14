ea620ef37304a0e2d627699462d0e181bd14ab8d
abhishek <abhi1kush@gmail.com>
2016-01-14 06:58:44 +0530

    checkboard impemented 20

---
+++ hashkeys.c
@@ -0,0 +1,34 @@
+//hashkey.c
+#include "header.h"
+#include <stdio.h>
+
+u64 generateposkey(const S_BOARD *pos)
+{
+  int sq=0;
+  u64 finalkey =0;
+  int piece = EMPTY;
+  //pieces 
+  for(sq=0;sq<SQ_NUM ; ++sq)
+  {
+    piece = pos->pieces[sq];
+    if(piece != OFFBOARD && piece!= EMPTY)
+    {
+      ASSERT(piece >= wp && piece <= bk);
+      finalkey ^=  piecekeys[piece][sq];
+    }
+  }
+
+  if(pos->side == WHITE)
+    finalkey ^= sidekey;
+  if(pos->enpass != NO_SQ)
+  {
+    ASSERT( pos->enpass >= 0 && pos->enpass <= SQ_NUM);
+    finalkey ^= piecekeys[EMPTY][pos->enpass];
+  }
+
+  ASSERT( pos->castle >= 0 && pos->castle <= 15);
+  finalkey ^= castlekeys[pos->castle];
+
+  return finalkey;
+
+}
